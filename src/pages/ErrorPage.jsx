49053765461fc1da3a9255f2a837d94851b7386e
ehsanhasan <mahmudulhasanehsan@gmail.com>
2024-08-30 20:45:55 +0600

    error page complete

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+import errorImg from '../assets/images/login/error.jpg'
 const ErrorPage = () => {
     return (
-        <div>
-            <h2 className='text-4xl text-center'>ops</h2>
+        <div className='my-auto mt-20'>
+            <img className=' md:w-1/3 h-[300px] md:h-[400px] mx-auto items-center justify-center  object-cover ' src={errorImg} alt="" />
+            <div className='text-center text-white text-lg mt-4 bg-orange-500 w-36 p-4 rounded mx-auto'>
+                <Link className=' ' to='/'>Go To Home</Link>
+            </div>
         </div>
     );
 };
