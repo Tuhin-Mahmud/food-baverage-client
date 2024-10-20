b961c6aa27c3cf7beeafe84f168d2bf0e379e7ec
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-10-20 17:19:49 +0600

    footer section complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,55 +1,74 @@
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaArrowRight, FaFacebook, FaLinkedin, FaTwitter, FaYoutube } from "react-icons/fa6";
+import { IoLocationSharp } from "react-icons/io5";
+import { MdOutgoingMail } from "react-icons/md";
+import { toast } from "react-toastify";
+
 const Footer = () => {
+    const handleSubmit = e => {
+        e.preventDefault()
+        toast.success('Thanks for subsCribe our news latter.')
+        // document.getElementById('myInputField').reset()
+    }
     return (
-        <div>
-            <footer className="footer footer-center bg-gray-100 text-base-content rounded p-10">
-                <nav className="grid grid-flow-col gap-4">
-                    <a className="link link-hover" href="">About us</a>
-                    <a className="link link-hover" href="/contact">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
-                </nav>
-                <nav>
-                    <div className="grid grid-flow-col gap-4">
-                        <a>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                className="fill-current">
-                                <path
-                                    d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-                            </svg>
-                        </a>
-                        <a>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                className="fill-current">
-                                <path
-                                    d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-                            </svg>
-                        </a>
-                        <a>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                className="fill-current">
-                                <path
-                                    d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-                            </svg>
-                        </a>
-                    </div>
-                </nav>
-                <aside>
-                    <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
-                </aside>
-            </footer>
-        </div>
+        <div className="bg-[#243642] text-white">
+
+            <div className="py-16">
+                <div className="md:flex  justify-around bg-base-200 text-base-content p-10 space-y-5">
+                    {/* about */}
+                    <aside className="text-center md:text-start md:w-1/5  space-y-2">
+                        <h1 className="text-4xl font-medium font-serif">About Us</h1>
+                        <p>Foodix is your go-to platform for  discovering and enjoying the best dining experiences   with a variety of delicious  options to  satisfy every craving.</p>
+                        <div className="flex items-center gap-7">
+                            <FaFacebook className="bg-gray-600 px-2  py-1 rounded text-4xl" />
+                            <FaTwitter className="bg-gray-600 px-2  py-1 rounded text-4xl" />
+                            <FaYoutube className="bg-gray-600 px-2  py-1 rounded text-4xl" />
+                            <FaLinkedin className="bg-gray-600 px-2  py-1 rounded text-4xl" />
+                        </div>
+                    </aside>
+                    {/* explore */}
+                    <nav className="flex flex-col text-center md:text-start space-y-2">
+                        <h6 className="text-4xl font-medium font-serif">Explore</h6>
+                        <a className="link link-hover flex items-center gap-1"><FaArrowRight className="text-sm" /> Company Profile</a>
+                        <a className="link link-hover flex items-center gap-1"><FaArrowRight className="text-sm" /> About</a>
+                        <a className="link link-hover flex items-center gap-1"><FaArrowRight className="text-sm" /> Help Center</a>
+                        <a className="link link-hover flex items-center gap-1"><FaArrowRight className="text-sm" /> Career</a>
+                        <a className="link link-hover flex items-center gap-1"><FaArrowRight className="text-sm" /> Contact</a>
+
+                    </nav>
+                    {/* contact info */}
+                    <nav className="flex flex-col text-center md:text-start space-y-2">
+                        <h6 className="text-4xl font-medium font-serif">Contact Info</h6>
+                        <a className="link link-hover flex gap-2 items-center"><IoLocationSharp className="bg-gray-600 px-2  py-1 rounded text-4xl  " />LA,4567
+                            Dhaka Uttara</a>
+                        <a className="link link-hover flex gap-2 items-center"><FaPhoneAlt className="bg-gray-600 px-2  py-1 rounded text-4xl  " />+880863528563</a>
+
+                        <a className="link link-hover flex gap-2 items-center"><MdOutgoingMail className="bg-gray-600 px-2  py-1 rounded text-4xl  " />Foodix@gamil.com</a>
+
+                    </nav>
+                    {/* news latter */}
+                    <nav className="flex flex-col text-center md:text-start space-y-2 ">
+                        <h6 className="text-4xl font-medium font-serif">NewsLetter</h6>
+                        <p>Join our subscribers list to <br /> get the latest news and special offers.</p>
+                        {/* input field */}
+                        <div>
+                            <form>
+                                <div className="flex items-center  mt-5">
+                                    <input id="myInputField" type="email" name="email" placeholder="Email" className="bg-gray-600 px-2 py-1 rounded focus-visible:outline-none border-b-2 w-full" />
+
+                                    <p onClick={handleSubmit} type="submit" className="cursor-pointer bg-slate-600 px-2 py-2 rounded-r-sm border-b-2 "><FaArrowRight className="" /></p>
+                                </div>
+                            </form>
+                        </div>
+                    </nav>
+                </div>
+                {/* bottom */}
+
+            </div >
+            <aside className="bg-gray-500 py-5 text-center text-sm text-white dark:bg-gray-800">
+                <p>&copy; Copyright 2024. Foodix All Rights Reserved.</p>
+            </aside>
+        </div >
     );
 };
 
